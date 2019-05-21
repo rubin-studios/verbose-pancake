cb5283c351632a266239c5b7c71eca1b42644b03
Dylan <dylan.swimwood@gmail.com>
2019-05-21 20:26:23 -0300

    Added charges and a cooldown to the grappling hook (#16)

--- Assets/Scripts/Player/GrapplingHook.cs
+++ Assets/Scripts/Player/GrapplingHook.cs
@@ -16,6 +16,7 @@ public class GrapplingHook : MonoBehaviour
     private RaycastHit2D hit;
     private bool m_Grappling = false;
     private float horizontalMove = 0f;
+    private int charges = 3;
 
 
     // Start is called before the first frame update
@@ -30,6 +31,8 @@ public class GrapplingHook : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        Debug.Log(charges);
+
         if (dj2d.distance > 1f) 
         {
             dj2d.distance -= step;
@@ -42,42 +45,51 @@ public class GrapplingHook : MonoBehaviour
             forceDirection.Normalize();
 
             // apply force
-            if (horizontalMove < 0.1f) 
+            if (horizontalMove < 0.0f) 
             {
                  rb2d.AddForce(forceDirection * force);
             }
-            else if (horizontalMove > 0.1f)
+            else if (horizontalMove > 0.0f)
             {
                 rb2d.AddForce(-forceDirection * force);
             }
+        }
+
+        if (charges <= 0) 
+        {
+            StartCoroutine("Cooldown");
         }
     }
 
     // Update is called once per frame
     public void Anchor()
     {   
-        targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        targetPos.z = 0.0f;
-
-        hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
-    
-        // Checks that the object hit is a rigid body
-        if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+        if (charges > 0)
         {
-            m_Grappling = true;
-            dj2d.enabled = true;
+            charges--;
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0.0f;
 
-            Vector2 connectPoint = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
-            connectPoint.x = connectPoint.x / hit.collider.transform.localScale.x;
-            connectPoint.y = connectPoint.y / hit.collider.transform.localScale.y;
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
+        
+            // Checks that the object hit is a rigid body
+            if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                m_Grappling = true;
+                dj2d.enabled = true;
 
-            dj2d.connectedAnchor = connectPoint;
-            dj2d.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
-            dj2d.distance = Vector2.Distance(transform.position, hit.point);
+                Vector2 connectPoint = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+                connectPoint.x = connectPoint.x / hit.collider.transform.localScale.x;
+                connectPoint.y = connectPoint.y / hit.collider.transform.localScale.y;
 
-            line.enabled = true;
-            line.SetPosition(0,transform.position);
-            line.SetPosition(1,hit.point);
+                dj2d.connectedAnchor = connectPoint;
+                dj2d.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                dj2d.distance = Vector2.Distance(transform.position, hit.point);
+
+                line.enabled = true;
+                line.SetPosition(0,transform.position);
+                line.SetPosition(1,hit.point);
+            }
         }
     }
 
@@ -101,5 +113,11 @@ public class GrapplingHook : MonoBehaviour
     public bool IsGrappling()
     {
         return m_Grappling;
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(3);
+        charges = 3;
     }
 }
